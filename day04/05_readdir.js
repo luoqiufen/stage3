5181fdd9e4814072cbbccfaebecde472bd0f78d2
luoqiufen <luoq6868@163.com>
2020-07-17 17:57:06 +0800

    day04

---
+++ day04/05_readdir.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-17 16:20:37
+ * @LastEditTime: 2020-07-17 17:45:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \stage3\day04\05_resddir.js
+ */ 
+var fs = require('fs');
+
+// 读取当前文件夹里面的内容
+/* fs.readdir('./',{withFileTypes:true},function(err,files){
+    console.log(err);
+    console.log(files);
+    console.log(files[0]);
+    console.log(files[0].name);
+    console.log(files[0].isFile());
+
+    for(var i=0;i<files.length;i++){
+        if(files[i].isFile()){
+            console.log(files[i].name+'是一个文件');
+        }else{
+            console.log(files[i].name+'是一个文件夹');
+        }
+    }
+}) */
+
+
+/* fs.readdir('./',function(err,files){
+    for(var i=0;i<files.length;i++){
+        fs.stat(files[i],function(err,state){
+            if(state.isFile()){
+                console.log(files.name+'是一个文件');
+            }else{
+                console.log(files.name+'是一个文件夹');
+            }  
+        })
+    }
+}) */
+
+fs.readdir('./',function(err,files){
+    
+
+    
+})
--- day04/test_01.js
+++ day04/test_01.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-07-17 14:52:48
- * @LastEditTime: 2020-07-17 15:37:03
+ * @LastEditTime: 2020-07-17 16:07:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \stage3\day04\test_01.js
@@ -13,15 +13,26 @@ var fs = require('fs')
 http.createServer(function (req, res) {
     var urlObj = url.parse(req.url, true);
     var path = urlObj.pathname;
-    
-    if(path=='/regist'){
+
+    if (path == '/regist') {
         var username = urlObj.query.username;
         var password = urlObj.query.password;
-        return;
+
+        var path = 'user.json';
+        var user = '[{"username":"' + username + '","password":"' + password + '"}]';
+
+        fs.writeFile(path, user, { flag: "w" }, function (err) {
+            if (err) {
+                res.end(err);
+                return;
+            }
+            res.end('写入成功');
+            return;
+        })
     }
 
-    fs.readFile('./test_01.html',function(err,data){
-        if(err){
+    fs.readFile('./test_01.html', function (err, data) {
+        if (err) {
             res.end("读取文件失败");
             return;
         }
@@ -29,18 +40,7 @@ http.createServer(function (req, res) {
     })
 
     res.setHeader('content-type', 'text/html;charset=utf-8');
-    var path = 'user.json';
-    var user = '[{"username":"'+username+'","password":"'+password+'"}]';
 
-    // var ops = { flag: 'a' }
-
-    fs.writeFile(path, user,{flag:"a"}, function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('写入成功');
-    })
 
 
 }).listen(4000);

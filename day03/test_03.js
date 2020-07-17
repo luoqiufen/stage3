9ba19e316a96cd4a38eda95c6101cd05185ec801
luoqiufen <luoq6868@163.com>
2020-07-17 10:29:44 +0800

    day03

--- day03/test_03.js
+++ day03/test_03.js
@@ -1,9 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-07-16 19:01:29
- * @LastEditTime: 2020-07-17 10:15:44
+ * @LastEditTime: 2020-07-17 10:29:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \2-coded:\web2004\stage3\day03\test_03.js
  */ 
-var 

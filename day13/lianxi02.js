0e11dc60aefad0b9689d14a34bbc51b4a42af625
YCKJ2532 <yuyehong@cloudwalk.cn>
2020-07-30 22:52:57 +0800

    aa

--- day13/lianxi02.js
+++ day13/lianxi02.js
@@ -12,11 +12,11 @@ app.use(cookieParser('keyboard cat'));
 app.get('/',function(req,res){
     var username = req.cookies.username;
     console.log(username);
-    /* if(username){//登录过了
+    if(username){//登录过了
         res.send('<h1>欢迎你,'+username+'</h1>');
         return;
-    } */
-    res.send('<form action="/login"><p>用户名:<input type="text" name="username"></p><p>密码:<input type="password" name="password"></p><input type="submit" value="登录"></form><a href="/regist">没有账号?点击注册</a>')
+    }
+    res.send('<form action="/login"><p>用户名:<input type="text" name="username"></p><p>密码:<input type="password" name="password"><span></span></p><input type="submit" value="登录"></form><a href="/regist">没有账号?点击注册</a>')
 })
 
 var userSchema = new mongoose.Schema({
@@ -35,8 +35,17 @@ mongoose.connect(url, opt)
 app.get('/login',function(req,res){
     var username = req.query.username;
     var password = req.query.password; 
+    if(!username || !password){
+        res.send('<h1 style="color:red;">用户名或密码不能为空!</h1>');
+        return;
+    }
+    
     // 查询数据库
     User.find({username},function(err,docs){
+        if(docs.length==0){
+            res.send('<h1 style="color:red;">用户名或密码错误！</h1>');
+            return;
+        }
         // console.log(docs[0].password);
         if(password == docs[0].password){
             res.cookie('username',username);
@@ -54,36 +63,29 @@ app.get('/regist',function(req,res){
 app.get('/register',function(req,res){
     var username = req.query.username;
     var password = req.query.password;
-    console.log(username);
-
-    User.find({},function(err,docs){
-        for(var i=0;i<docs.length;i++){
-            console.log(docs[i].username);
-            if(username == docs[i].username){
-                res.send('用户名已存在,请重新输入');
-                res.end();
-                return;
-            }else{
-                res.cookie('username',username);
-                res.send('<h1>注册成功! 欢迎你,'+username+'</h1>');
-                return;
-            }
+    if(!username || !password){
+        res.send('<h1 style="color:red;">用户名或密码不能为空!</h1>');
+        return;
+    }
+    User.find({username},function(err,docs){   
+        if(docs.length!=0){
+            res.send('<h1 style="color:red;">用户名已存在,请重新输入<h1>');
+            return;
         }
+        // 用户名不存在,注册
         var user = {
             username:username,
             password:password
         }
-        var a = new User(user);
-        a.save(function (error, doc) {
-            console.log(error)
-            console.log(doc)
-        })
+
         User.insertMany([user],function(error,res){
-            console.log(error);
-            console.log(res);
+            // console.log(error);
+            // console.log(res);
         })
 
-
+        res.cookie('username',username);
+        res.send('<h1>注册成功! 欢迎你,'+username+'</h1>');
+        return;
     })
 
 })

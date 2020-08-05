2ffd7ac5d101cb962e18c5e799a11f48428676b9
luoqiufen <luoq6868@163.com>
2020-08-05 20:16:50 +0800

    day16

--- day16/lianxi.js
+++ day16/lianxi.js
@@ -3,8 +3,8 @@
 // 1.定义一个方法,根据传入的参数,计算得出参数的总和(使用扩展运算符)
 {
     let sum = (...arr) => {
-        var sum = 0;
-        for (var i = 0; i < arr.length; i++) {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
         return sum;
